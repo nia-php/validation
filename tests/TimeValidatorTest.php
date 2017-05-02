ebc2afe7a9b917037cb1b00a44feb022e1791af7
Patrick Ullmann <patrick.ullmann@nat-software.de>
2017-05-02 12:19:56 +0200

    PHPUnit tests upgraded to PHPUnit 5.4+

--- tests/TimeValidatorTest.php
+++ tests/TimeValidatorTest.php
@@ -10,7 +10,7 @@
 declare(strict_types = 1);
 namespace Test\Nia\Validation;
 
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 use Nia\Validation\TimeValidator;
 use Nia\Collection\Map\StringMap\Map;
 use Nia\Validation\Violation\Violation;
@@ -18,7 +18,7 @@ use Nia\Validation\Violation\Violation;
 /**
  * Unit test for \Nia\Validation\TimeValidator.
  */
-class TimeValidatorTest extends PHPUnit_Framework_TestCase
+class TimeValidatorTest extends TestCase
 {
 
     /**
